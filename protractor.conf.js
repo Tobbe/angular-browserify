e55ceeb491b1786b1aaee34be8e602dd3eff3147
Tobbe Lundberg <tobbe@tlundberg.com>
2016-08-10 17:16:04 +0200

    clean up protractor config

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,18 +1,4 @@
 exports.config = {
-
-  chromeOnly: true,
-  chromeDriver: './node_modules/protractor/selenium/chromedriver',
-
-  capabilities: {
-    'browserName': 'chrome'
-  },
-
-  specs: ['e2e/**/*_spec.js'],
-
+  directConnect: true,
   rootElement: '.content',
-
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000
-  }
 };
