54e5ec19d6578cd8ca2b0d86f6376b4d6d247c88
Tobbe Lundberg <tobbe@tlundberg.com>
2016-08-11 14:42:54 +0200

    (protractor) Set Chrome options to get rid of annoying popup

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,4 +1,8 @@
 exports.config = {
     directConnect: true,
     rootElement: '.content',
+    capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': {'args': ['--disable-extensions --disable-web-security --disk-cache-size=1 --media-cache-size=1']}
+    },
 };
